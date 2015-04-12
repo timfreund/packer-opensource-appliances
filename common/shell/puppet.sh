512628f532d3f703af9bddd90abd9045dc48c2a7
Tim Freund <tim@freunds.net>
2015-04-12 00:29:12 -0400

    only download puppetlabs-release if it doesn't exist

--- common/shell/puppet.sh
+++ common/shell/puppet.sh
@@ -4,7 +4,11 @@ version = $1
 echo "packer ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/packer
 chmod 440 /etc/sudoers.d/packer
 
-wget http://apt.puppetlabs.com/puppetlabs-release-${version}.deb
+if [ ! -f puppetlabs-release-${version}.deb ]
+then
+    wget http://apt.puppetlabs.com/puppetlabs-release-${version}.deb
+fi
+
 dpkg -i puppetlabs-release-${version}.deb
 apt-get update
 
