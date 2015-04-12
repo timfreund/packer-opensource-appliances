c3e50e732561ac2da9cbf3d1cf324a890ac8b7e7
Tim Freund <tim@freunds.net>
2015-04-12 00:28:41 -0400

    Add command line options to build-machine.sh to support machine name and builder name

--- build-machine.sh
+++ build-machine.sh
@@ -1,7 +1,53 @@
 #!/usr/bin/env bash
 
-name=$1
+MACHINENAME=unknown
+MACHINEDIR=`dirname ${0}`/machines
+BUILDER=all
 
+usage()
+{
+cat<<EOF
+usage : $0 options
+
+Build a machine with packer using default variable values
+
+OPTIONS:
+  -h      Show this message
+  -m      The machine name
+  -b      The builder (all builders is the default)
+
+EOF
+}
+
+while getopts "b:m:h" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 0
+            ;;
+        m)
+            MACHINENAME=${OPTARG}
+            DESTDIR=${MACHINEDIR}/${MACHINENAME}
+            ;;
+        b)
+            BUILDER=${OPTARG}
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [[ ${MACHINENAME} == "unknown" ]]
+then
+    echo "A machine name is required"
+    usage
+    exit 2
+fi
+
+set -x
 cmd="packer build"
 
 for vf in common ubuntu ubuntu14.04;
@@ -9,7 +55,16 @@ do
     cmd="${cmd} -var-file=../../common/variables-${vf}.json"
 done
 
-cmd="${cmd} -only qemu machine.json"
+if [[ ${BUILDER} != "all" ]]
+then
+    cmd="${cmd} -only ${BUILDER}"
+fi
+cmd="${cmd} machine.json"
 
-cd machines/${name}
+cd ${DESTDIR}
+if [ -e Puppetfile ]
+then
+    r10k -v DEBUG puppetfile install
+fi
+pwd
 $cmd
